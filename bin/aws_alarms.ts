471b8429ac225dcdd830a4af1544a2b073f8cd4c
judeimuede <judeimuede@gmail.com>
2021-01-20 16:30:41 +0200

    initial commits

--- bin/aws_alarms.ts
+++ bin/aws_alarms.ts
@@ -4,4 +4,9 @@ import * as cdk from '@aws-cdk/core';
 import { AwsAlarmsStack } from '../lib/aws_alarms-stack';
 
 const app = new cdk.App();
-new AwsAlarmsStack(app, 'AwsAlarmsStack');
+new AwsAlarmsStack(app, 'AwsAlarmsStack', {
+    env: {
+        account: '296274010522',
+        region: 'us-east-1',
+    }
+});
--- lib/aws_alarms-stack.ts
+++ lib/aws_alarms-stack.ts
@@ -1,9 +1,36 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as cloudwatch from '@aws-cdk/aws-cloudwatch';
 
 export class AwsAlarmsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Metric class
+    const metric = new cloudwatch.Metric({
+      namespace: 'AWS/EC2',
+      metricName: 'CPUUtilization',
+      statistic: 'Average',
+      period: cdk.Duration.minutes(5),
+      dimensions: [
+        {
+        name: 'Instance',
+        value: 'i-0e7e9ac3b9ba678d2'
+      },
+    ],
+    });
+
+    // Alarm class
+    const alarm = new cloudwatch.Alarm(this, 'Alarm', {
+      metric: metric,
+      threshold: 100,
+      evaluationPeriods: 3,
+      datapointsToAlarm: 2,
+      actionsEnabled: true,
+      alarmDescription: 'Alarm when CPU Utilization exceeds 100%',
+    
+      
+
+    });
   }
 }
